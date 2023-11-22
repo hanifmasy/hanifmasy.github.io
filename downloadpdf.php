8a56526f7982cb2d09c29517625cd81bd57d22dc
hanifmasy <86345153+hanifmasy@users.noreply.github.com>
2023-11-22 15:26:59 +0800

    Update downloadpdf.php

--- downloadpdf.php
+++ downloadpdf.php
@@ -1,3 +1,9 @@
 <?php
- header('Content-disposition: attachment; filename=cv_hanif_masykuri.pdf');
- header('Content-type: application/pdf');
+     header("Cache-Control: public");
+     header("Content-Description: File Transfer");
+     header("Content-Disposition: attachment; filename=cv_hanif_masykuri.pdf");
+     header("Content-Type: application/pdf");
+     header("Content-Transfer-Encoding: binary");
+     readfile("cv_hanif_masykuri.pdf");
+ ?>
+
