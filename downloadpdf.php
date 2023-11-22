3c389db986c30a2a444eb02c697ac40cf96088d4
hanifmasy <86345153+hanifmasy@users.noreply.github.com>
2023-11-22 15:32:22 +0800

    Delete downloadpdf.php

--- downloadpdf.php
+++
@@ -1,9 +0,0 @@
-<?php
-     header("Cache-Control: public");
-     header("Content-Description: File Transfer");
-     header("Content-Disposition: attachment; filename=cv_hanif_masykuri.pdf");
-     header("Content-Type: application/pdf");
-     header("Content-Transfer-Encoding: binary");
-     readfile("cv_hanif_masykuri.pdf");
- ?>
-
